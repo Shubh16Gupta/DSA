eb5c1e3710838215cfb1e23a0ad84aed1c010129
Shubh Gupta <2008shubhgupta@gmail.com>
2026-06-07 16:44:09 +0530

    Time: 4 ms (12.10%) | Memory: 28.4 MB (32.57%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.cpp
+++ 3226-minimum-number-game/minimum-number-game.cpp
@@ -3,10 +3,16 @@ public:
     vector<int> numberGame(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         vector<int> arr ; 
-        for(int i = 0 ; i < nums.size() ; i+=2 ){
-            arr.push_back(nums[i + 1]);
+        for(int i = 0 ; i < nums.size() ; i++ ){
+            if (i % 2 == 0) {
 
-            arr.push_back(nums[i]);
+                arr.push_back(nums[i + 1]);
+
+            } else {
+
+                arr.push_back(nums[i - 1]);
+
+            }
         }
         return arr;
 
