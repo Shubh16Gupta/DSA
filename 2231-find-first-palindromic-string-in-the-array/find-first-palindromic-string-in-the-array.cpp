7a2d3d4a1a0dbbcab30e23d1c28911e713f320ef
Shubh Gupta <2008shubhgupta@gmail.com>
2026-06-10 18:10:29 +0530

    Time: 8 ms (33.16%) | Memory: 30 MB (11.50%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
+    bool isPalindrome(string s){
+        int left = 0 , right = s.size()-1 ; 
+        while(left<right){
+            if (s[left] != s[right]){
+                return false ; 
+            }
+            left++ ; 
+            right -- ; 
+        }
+        return true ; 
+
+    }
     string firstPalindrome(vector<string>& words) {
+        //brute one
+        // for(string s : words){
+        //     string og = s ; 
+        //     string temp = s ;
+        //     reverse(temp.begin(), temp.end()); 
+        //     if(og == temp ){
+        //         return s ;
+        //         break ;  
+        //     }
+        // }
+       // return "";
+
+        //optimal 
         for(string s : words){
-            string og = s ; 
-            string temp = s ;
-            reverse(temp.begin(), temp.end()); 
-            if(og == temp ){
+            if(isPalindrome(s)){
                 return s ;
-                break ;  
             }
         }
-        return "";
+        return "" ; 
         
     }
 };
