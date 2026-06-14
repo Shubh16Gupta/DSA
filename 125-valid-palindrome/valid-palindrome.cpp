62e0cc7012ba7063cdeefcb8a61d8cbe883d78ec
Shubh Gupta <2008shubhgupta@gmail.com>
2026-06-14 16:43:26 +0530

    Time: 2 ms (36.35%) | Memory: 10.2 MB (24.50%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string result = "" ; 
+        for( char ch : s){
+            if(isalnum(ch)){
+                result += tolower(ch) ; 
+            }
+        }
+
+
+        int left = 0 ;
+        int right = result.size() - 1 ; 
+
+        while(left < right){
+            if(result[left] != result[right]){
+                return false ; 
+            }
+            left ++ ; 
+            right -- ; 
+        }
+
+        return true ; 
+
+
+
+        
+    }
+};
