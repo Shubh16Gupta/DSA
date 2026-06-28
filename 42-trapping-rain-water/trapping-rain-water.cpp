de20dd646b9297b249cbf166db3c8783cfca73a1
Shubh Gupta <2008shubhgupta@gmail.com>
2026-06-28 23:21:17 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (17.33%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -7,13 +7,13 @@ public:
         vector<int> leftMax(n);
         vector<int> rightMax(n);
 
-        // Build leftMax
+        
         leftMax[0] = height[0];
         for (int i = 1; i < n; i++) {
             leftMax[i] = max(leftMax[i - 1], height[i]);
         }
 
-        // Build rightMax
+        
         rightMax[n - 1] = height[n - 1];
         for (int i = n - 2; i >= 0; i--) {
             rightMax[i] = max(rightMax[i + 1], height[i]);
@@ -21,7 +21,7 @@ public:
 
         int ans = 0;
 
-        // Calculate trapped water
+        
         for (int i = 0; i < n; i++) {
             ans += min(leftMax[i], rightMax[i]) - height[i];
         }
