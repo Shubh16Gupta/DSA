e333aa2fe3c0ff2630091c172432e2c093fed615
Shubh Gupta <2008shubhgupta@gmail.com>
2026-07-01 15:09:40 +0530

    Time: 3 ms (74.12%) | Memory: 48.6 MB (61.39%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        vector<int>freq(nums.size() + 1, 0) ; 
-        vector<int>ans ;
+        vector<int>ans; 
         for(int i = 0 ; i < nums.size() ; i++){
-            freq[nums[i]]++ ;       
-        }
-        for(int i = 1 ; i < freq.size() ; i++){
-            if(freq[i] > 1){
-                ans.push_back(i);
+            int index  = abs(nums[i])-1 ; 
+            if ( nums[index] < 0){
+                ans.push_back(abs(nums[i]));
+            }else{
+                nums[index] = -nums[index] ; 
             }
         }
         return ans ; 
